3235625c676a1f82ee60d3da392356d2098c7192
Brandon Morales <jessybrandon@hotmail.com>
2018-11-24 17:49:05 -0600

    fin de la seccion 6

---
+++ portafolio/src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,10 @@
+
+export interface InfoPagina {
+  titulo?: string;
+  email?: string;
+  nombre?: string;
+  facebook?: string;
+  instagram?: string;
+  tumbler?: string;
+  equipo_trabajo?: any[];
+}
---
+++ portafolio/src/app/services/info-pagina.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { InfoPagina } from "../interfaces/info-pagina.interface";
+
+@Injectable({
+  providedIn: "root"
+})
+export class InfoPaginaService {
+  info: InfoPagina = {};
+  cargada = false;
+
+  constructor(private http: HttpClient) {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo() {
+    // Leer el archivo JSON
+    this.http
+      .get("assets/data/data-pagina.json")
+      .subscribe((resp: InfoPagina) => {
+        this.cargada = true;
+        this.info = resp;
+      });
+  }
+
+  private cargarEquipo(){
+    this.http
+      .get("https://angular-html-b3a90.firebaseio.com/equipo.json")
+      .subscribe( (resp: any[]) => {
+        this.equipo = resp;
+        // console.log(resp);
+      });
+  }
+}
